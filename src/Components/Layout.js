d837f726692989600f1e3d10568753ccb42d04a9
Yaryna Korduba <yaryna.korduba@stud.uni-bamberg.de>
2018-06-20 22:02:23 +0200

    deleted unnecessary

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,15 +1,14 @@
-import React from "react";
+import React, {Fragment} from "react";
 import Calendar from "./Calendar";
 import AddOutlayForm from "./AddOutlayForm";
 import { base } from "../firebase";
-import {filter, isEmpty, defaultTo, pipe, keys, map, values, reduceBy, add, groupBy} from "ramda";
+import {filter, isEmpty, pipe, keys, map, values, reduceBy, add, groupBy} from "ramda";
 import {v1} from "react-native-uuid";
 import {format, getDate, getMonth, getYear} from "date-fns";
 
 
 var reduceByDateAttribute = reduceBy((acc, item) => add(acc,+item.amount), 0);
 var reduceByDateValue = reduceBy((acc, item) => add(acc,+values(item)), 0);
-
 var sumByDate = reduceByDateAttribute((item) => item.date);
 var sumByMonth = reduceByDateValue((item) => format(keys(item), "YYYY-MM"));
 var sumByYear = reduceByDateValue((item) => getYear(keys(item)));
@@ -26,7 +25,6 @@ class Layout extends React.Component {
   state = { sorted_list: [] };
 
   componentDidMount() {
-
       this.cost_listRef = base.syncState("sorted_list", {
           context: this,
           state: "sorted_list"
@@ -39,47 +37,42 @@ class Layout extends React.Component {
 
 
   reduceByDates = () => {
-      var temp1 = new Array();
-      pipe(
+      var amountsForPeriods = new Array();
+      pipe (
           map(
               pipe(
-                  map(map((el) => (temp1[keys(sumByDate(el))[0]] =
-                      values(sumByDate(el))[0],sumByDate(el)))),
-                  map(el1 => (temp1[keys(sumByMonth(values(el1)))[0]] =
-                      values(sumByMonth(values(el1)))[0], sumByMonth(values(el1))))
+                  map(map((date) => (amountsForPeriods[keys(sumByDate(date))[0]] =
+                      values(sumByDate(date))[0],sumByDate(date)))),
+                  map(month => (amountsForPeriods[keys(sumByMonth(values(month)))[0]] =
+                      values(sumByMonth(values(month)))[0], sumByMonth(values(month))))
               )),
-          map(el => (temp1[keys(sumByYear(values(el)))[0]] =
-              values(sumByYear(values(el)))[0], sumByYear(values(el)))),
-          (el => (temp1["total"] = values(sumTotal(values(el)))[0])),
+          map(year => (amountsForPeriods[keys(sumByYear(values(year)))[0]] =
+              values(sumByYear(values(year)))[0], sumByYear(values(year)))),
+          (total => (amountsForPeriods["total"] = values(sumTotal(values(total)))[0])),
           keys
       )(groupByDates(this.state.sorted_list));
-      return temp1;
+      return amountsForPeriods;
   };
 
 
   addOutlay = item => {
-    this.setState({
-    sorted_list: [item, ...this.state.sorted_list]
-    })
+    this.setState({sorted_list: [item, ...this.state.sorted_list]})
   };
 
-  deleteOutlay = (id) => {
-    let temp_list = filter(i => i.id !== id, this.state.sorted_list);
-    this.setState({
-    sorted_list: temp_list
-    })
+  deleteOutlay = id => {
+    this.setState({sorted_list: filter(i => i.id !== id, this.state.sorted_list)})
   };
 
   render() {
     const { sorted_list } = this.state;
-    var amountsForPeriods = !isEmpty(sorted_list) ? this.reduceByDates() : null;
+    var amountsForPeriods = this.reduceByDates();
     return (
-      <div>
+      <Fragment>
         <AddOutlayForm addOutlay={this.addOutlay} />
         <Calendar cost_list={isEmpty(sorted_list) ? sorted_list : groupByDates(sorted_list)}
                   deleteOutlay={this.deleteOutlay}
-        amountsForPeriods={!isEmpty(sorted_list) ? this.reduceByDates() : null}/>
-      </div>
+        amountsForPeriods={!isEmpty(sorted_list) ? amountsForPeriods : null}/>
+      </Fragment>
     )
   }
 }
