22d50db14634325edc561e27ff499659f110c984
Yaryna Korduba <yaryna.korduba@stud.uni-bamberg.de>
2018-07-25 14:18:49 +0200

    changing the calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -4,7 +4,7 @@ import {Month} from './Month'
 import {Day} from './Day'
 import {Outlay} from './Outlay'
 import PropTypes from 'prop-types'
-import {compose, map, reverse, prop, sum, filter, groupBy, mapObjIndexed, values} from 'ramda'
+import {compose, map, reverse, prop, sum, filter, groupBy, mapObjIndexed, values, tap} from 'ramda'
 import {
     isWithinInterval,
     startOfDay,
@@ -18,6 +18,17 @@ import {
     getDate
 } from "date-fns"
 
+const getAmountByInterval = interval =>  compose(
+    sum,
+    map(prop("amount")),
+    filter(({ date }) => isWithinInterval(date, interval))
+)
+const getTotalAmount = getAmountByInterval({ start: new Date(0), end: new Date() });
+const getAmountByYear = date => getAmountByInterval({ start: startOfYear(date), end: endOfYear(date) })
+const getAmountByMonth = date => getAmountByInterval({ start: startOfMonth(date), end: endOfMonth(date) })
+const getAmountByDay = date => getAmountByInterval({ start: startOfDay(date), end: endOfDay(date) })
+const months = [1,2,3,4,5,6,7,8,9,10,11,12];
+
 const iterateBy = (groupingFn, iteratorFn) =>
     compose(
         //prettier-ignore
@@ -26,15 +37,15 @@ const iterateBy = (groupingFn, iteratorFn) =>
         mapObjIndexed(iteratorFn),
         groupBy(({ date }) => groupingFn(date))
     );
-const getAmountByInterval = interval =>  compose(
-    sum,
-    map(prop("amount")),
-    filter(({ date }) => isWithinInterval(date, interval))
-);
-const getTotalAmount = getAmountByInterval({ start: new Date(0), end: new Date() });
-const getAmountByYear = date => getAmountByInterval({ start: startOfYear(date), end: endOfYear(date) })
-const getAmountByMonth = date => getAmountByInterval({ start: startOfMonth(date), end: endOfMonth(date) })
-const getAmountByDay = date => getAmountByInterval({ start: startOfDay(date), end: endOfDay(date) })
+const filterMonth = (month) => (item) => {
+    return getMonth(new Date(item.date)) === month};
+
+
+const iterByMonth = (items) => map(compose(
+    map(el => <Month key={el.id} date={el.date} titleLabel={getAmountByMonth(el.date)(items)}>{el}</Month>),
+    month => filter(filterMonth(month),items),
+), months);
+
 
 
 const Calendar = ({outlays, onOutlayClick}) => (
@@ -43,7 +54,16 @@ const Calendar = ({outlays, onOutlayClick}) => (
                 <h3>Total amount spent: {getTotalAmount(outlays)}$</h3>
                 {iterateBy(getYear, items => (
                     <Year key={items[0].date} date={items[0].date} titleLabel={getAmountByYear(items[0].date)(items)}>
-                        {iterateBy(getMonth, items => (
+                        {console.log("~~~~", iterByMonth(items))}
+                    </Year>
+                ))(outlays)}
+            </div>
+);
+
+
+/*
+*                         {
+                            iterateBy(getMonth, items => (
                             <Month key={items[0].date} date={items[0].date}
                                    titleLabel={getAmountByMonth(items[0].date)(items)}>
                                 {iterateBy(getDate, items => (
@@ -60,10 +80,8 @@ const Calendar = ({outlays, onOutlayClick}) => (
                                 ))(items)}
                             </Month>
                         ))(items)}
-                    </Year>
-                ))(outlays)}
-            </div>
-);
+*
+* */
 
 Calendar.contextTypes = {
     store: PropTypes.object,
--- src/actions/index.js
+++ src/actions/index.js
@@ -10,7 +10,6 @@ export const deleteOutlay = (id) => (dispatch) => (
             response
         })
     )
-
     );
 
 export const addOutlay = (title, amount, date) => (dispatch) =>
