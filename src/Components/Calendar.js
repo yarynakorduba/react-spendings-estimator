64ad92a2da5ce5948e6f8e6cf1083f9755e17d1b
Yaryna Korduba <yaryna.korduba@stud.uni-bamberg.de>
2018-07-25 15:45:02 +0200

    fixing calendar

--- src/Components/Calendar.js
+++ src/Components/Calendar.js
@@ -4,7 +4,7 @@ import {Month} from './Month'
 import {Day} from './Day'
 import {Outlay} from './Outlay'
 import PropTypes from 'prop-types'
-import {compose, map, reverse, prop, sum, filter, groupBy, mapObjIndexed, values, tap} from 'ramda'
+import {compose, map, reverse, prop, sum, filter, groupBy, isEmpty, mapObjIndexed, values, tap, indexOf} from 'ramda'
 import {
     isWithinInterval,
     startOfDay,
@@ -22,7 +22,7 @@ const getAmountByInterval = interval =>  compose(
     sum,
     map(prop("amount")),
     filter(({ date }) => isWithinInterval(date, interval))
-)
+);
 const getTotalAmount = getAmountByInterval({ start: new Date(0), end: new Date() });
 const getAmountByYear = date => getAmountByInterval({ start: startOfYear(date), end: endOfYear(date) })
 const getAmountByMonth = date => getAmountByInterval({ start: startOfMonth(date), end: endOfMonth(date) })
@@ -38,13 +38,19 @@ const iterateBy = (groupingFn, iteratorFn) =>
         groupBy(({ date }) => groupingFn(date))
     );
 const filterMonth = (month) => (item) => {
-    return getMonth(new Date(item.date)) === month};
+    console.log(getMonth(new Date(item.date)) === month);
+    return getMonth(new Date(item.date)) === month;
+};
 
 
-const iterByMonth = (items) => map(compose(
-    map(el => <Month key={el.id} date={el.date} titleLabel={getAmountByMonth(el.date)(items)}>{el}</Month>),
-    month => filter(filterMonth(month),items),
-), months);
+const iterByMonth = (items) => compose(
+    map((month) => console.log("^^^^^^^^^^^^", new Date(0, month.key[0], 0)) ||
+        <Month key={month.key} date={new Date(0, month.key, 0)} titleLabel={isEmpty(month.value) ? 0 :
+            getAmountByMonth(month.value[0].date)(items)}></Month>
+    ),
+    tap(el => console.log(el)),
+    map(month => {return {key: [month], value: filter(filterMonth(month),items)}}),
+    );
 
 
 
@@ -54,7 +60,7 @@ const Calendar = ({outlays, onOutlayClick}) => (
                 <h3>Total amount spent: {getTotalAmount(outlays)}$</h3>
                 {iterateBy(getYear, items => (
                     <Year key={items[0].date} date={items[0].date} titleLabel={getAmountByYear(items[0].date)(items)}>
-                        {console.log("~~~~", iterByMonth(items))}
+                        {iterByMonth(items)(months)}
                     </Year>
                 ))(outlays)}
             </div>
